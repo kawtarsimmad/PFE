f73f3a65aca8faab5a1174e65b20325fcaca0eb2
ilhame <ilhamehoumadi15@gmail.com>
2024-04-24 12:31:41 +0100

    categoty

--- project/categories/models.py
+++ project/categories/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+
+    
+    id=models.AutoField(primary_key=True)
+    name=models.CharField(max_length=50)
+    
+    def __str__(self) :
+        return self.name
+    class Meta:
+        #verbose_name = '',
+        ordering =['name']
+        
--- project/categories/urls.py
+++ project/categories/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    
-]
+from django.shortcuts import render
+from . models import Category
+# Create your views here.
+def category(request):
+    return render(request, 'categories/category.html')
+def categories(request):
+    return render(request, 'categories/categories.html',{'ct':Category.objects.all()})
