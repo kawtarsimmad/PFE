3149b433684119579e5643a663dd1e329cf2ca8a
kawtarsimmad <kawtarsimmad02@gmail.com>
2024-04-24 20:29:35 +0100

    upadte forms
    add status

--- project/reclamations/forms.py
+++ project/reclamations/forms.py
@@ -4,7 +4,9 @@ from .models import Reclamation
 
 class ReclamationForm(forms.ModelForm):
     recl_type= forms.ChoiceField(choices=Reclamation.RECLAMATION_TYPES, widget=forms.Select(attrs={"class":"forms-control", "placeholder": "recl_type"}))
-    description= forms.CharField(widget=forms.Textarea(attrs={"class":"forms-control", "placeholder": "description"}))    
+    description= forms.CharField(widget=forms.Textarea(attrs={"class":"forms-control", "placeholder": "description"}))
+    status=forms.CharField(widget=forms.TextInput(attrs={"class":"forms-control", "placeholder": "status"}))
+    
     class Meta:
             model = Reclamation
-            fields = ['recl_type', 'description']
+            fields = ['recl_type', 'description', 'status']
