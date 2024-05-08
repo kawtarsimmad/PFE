07d5ce2776fc04aac5bbe064fde386f5d592cd68
kawtarsimmad <kawtarsimmad02@gmail.com>
2024-05-08 08:55:45 +0100

    update index
    posts are shoings now

--- project/pages/views.py
+++ project/pages/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from publications.models import Publication
+
 
 # Create your views here.
 def index(request):
-    return render(request ,'pages/index.html')
+    publications = Publication.objects.all()  # Fetch all publications
+    return render(request, 'pages/index.html', {'publications': publications})
