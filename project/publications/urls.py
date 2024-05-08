523efdbded9e9cb7b72291ead5c6cb16a7b21703
kawtarsimmad <kawtarsimmad02@gmail.com>
2024-05-08 09:56:02 +0100

    publication
    images affiche

--- project/publications/urls.py
+++ project/publications/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 
+
 urlpatterns = [
     path('publication/',views.publication,name='publication'),
     path('publications/',views.publications,name='publications'),
