d6638f36776d013421b1ca075172c6725018b125
kawtarsimmad <kawtarsimmad02@gmail.com>
2024-05-08 19:32:22 +0100

    venv

--- project/pages/urls.py
+++ project/pages/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('', views.get_nearby_associations, name='get_nearby_associations'),
     # Ajoutez d'autres patterns d'URL au besoin
 ]
--- project/pages/views.py
+++ project/pages/views.py
@@ -2,8 +2,6 @@ from django.shortcuts import render
 from publications.models import Publication
 from django.http import JsonResponse
 from django.views.decorators.http import require_GET
-from django.contrib.gis.db.models.functions import Distance
-from django.contrib.gis.geos import Point
 from users.models import Association
 
 
@@ -13,19 +11,4 @@ def index(request):
     publications = Publication.objects.all()  # Fetch all publications
     return render(request, 'pages/index.html', {'publications': publications})
 
-@require_GET
-def get_nearby_associations(request):
-    latitude = request.GET.get('latitude')
-    longitude = request.GET.get('longitude')
 
-    user_location = Point(float(longitude), float(latitude), srid=4326)  # Create Point object
-    associations = Association.objects.annotate(distance=Distance('location', user_location)).order_by('distance')[:10]
-
-    data = [{
-        'name': association.name,
-        'distance': association.distance.km,
-        'address': association.address,
-        # Add more fields as needed
-    } for association in associations]
-
-    return JsonResponse(data, safe=False)
